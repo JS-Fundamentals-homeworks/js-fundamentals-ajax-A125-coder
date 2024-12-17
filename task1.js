43d0d043cb0a3a7e9cc855270fbd434bbe0e9f61
Anna Romanovska <sup2a1nn@gmail.com>
2024-12-17 19:21:31 +0200

    Solved tasks

--- task1.js
+++ task1.js
@@ -1,7 +1,20 @@
-// Завдання: отримання даних про користувачів 
+// Завдання: отримання даних про користувачів
 // За допомогою засобі Fetch отримати інформацію про користувачів
-// за посиланням - https://jsonplaceholder.typicode.com/users 
+// за посиланням - https://jsonplaceholder.typicode.com/users
 // Імена користувачів відобразити в ненумерованому списку ul.usersList,
 // який створений у файлі index.html
 // Запустити програму за допомогою Live Server
-// Перевірити за допомогою команди npm tests/task1.test.js 
+// Перевірити за допомогою команди npm tests/task1.test.js
+
+const userList = document.querySelector('ul.usersList');
+
+(async function getUserNames() {
+  let result = await fetch("https://jsonplaceholder.typicode.com/users");
+  let data = await result.json();
+  let userItems = "";
+  data.forEach((user) => {
+    userItems += `<li>${user.name}</li>`;
+  });
+  userList.innerHTML = userItems;
+})();
+
--- task2.js
+++ task2.js
@@ -5,3 +5,18 @@
 // відобразити у тезі #userCity
 // Запустити програму потрібно за допомогою Live Server
 // Перевірити правильність програми - команда node tests/task2.test.js
+
+const userNameInput = document.getElementById("userNameInput");
+const getuserBtn = document.getElementById("getUserButton");
+let userCity = document.getElementById("userCity");
+
+getuserBtn.addEventListener('click', async () => {
+    userCity.innerHTML = "";
+    let result = await fetch("https://jsonplaceholder.typicode.com/users");
+    let data = await result.json();
+    data.find(user => {
+        if(user.name === userNameInput.value){
+            userCity.innerHTML = `${user.address.city}`;
+        }
+    })
+})
